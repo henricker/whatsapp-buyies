df13656a28d229e49a2273ccb3914cf3be72d244
henricker <henriquevieira@alu.ufc.br>
2024-04-15 10:14:42 -0300

    feat: init whatsapp

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+// config do db
+// renomeie esse arquivo para config.js
+export default {
+  user: 'postgres', // usuarios do db
+  host: 'localhost', // host do db
+  database: 'whatsapp', // qual db usar
+  password: 'whatsapp', // senha
+  port: 5434, // porta padrão do Postgres
+}
